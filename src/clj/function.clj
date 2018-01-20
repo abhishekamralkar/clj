0daac1a79697afca75bb8fb1b3a8f7e14a1430c2
Piyush Katariya <corporate.piyush@gmail.com>
2018-01-20 14:15:22 +0530

    type hints perf

--- src/clj/function.clj
+++ src/clj/function.clj
@@ -25,11 +25,40 @@
 
 ;; Contract based programming - function with pre post condition
 (defn add [a b]
-    {:pre [(pos? a) (pos? b)]
-     :post [(< (* a b))]}
-    (+ a b))
+  {:pre [(pos? a) (pos? b)]
+   :post [(< (* a b))]}
+  (+ a b))
 
 (add -2 -3)
 (add 2 -3)
 (add 2 -3)
 (add 2 3)
+
+;; type hints performance
+
+(defn len [x]
+  (.length x))
+
+(time (reduce + (map len (repeat 1000000 "asdf"))))
+
+(defn len1 [^String x]
+  (.length x))
+
+(time (reduce + (map len1 (repeat 1000000 "asdf"))))
+
+(defn foo [n]
+  (loop [i 0]
+    (if (< i n)
+      (recur (inc i))
+      i)))
+
+(time (foo 100000))
+
+(defn foo2 [n]
+  (let [n (int n)]
+    (loop [i (int 0)]
+      (if (< i n)
+        (recur (inc i))
+        i))))
+
+(time (foo2 100000))
